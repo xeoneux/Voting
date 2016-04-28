887ee073738ebe0a33ee3e1d965bcf54f528e07d
Aayush Kapoor <xeoneux@icloud.com>
2016-04-29 03:43:35 +0530

    Add logic to end the vote

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -81,6 +81,23 @@ describe('application logic', () => {
         entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
       }));
     });
+
+    it('marks winner when just one entry left', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        winner: 'Trainspotting'
+      }));
+    });
   });
 
   describe('vote', () => {
