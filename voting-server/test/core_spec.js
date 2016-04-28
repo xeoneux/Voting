35f4949eaf204531448dacd7e18a8864a35e113c
Aayush Kapoor <xeoneux@icloud.com>
2016-04-28 22:45:37 +0530

    Add logic to load entries

---
+++ voting-server/test/core_spec.js
@@ -0,0 +1,28 @@
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+import {setEntries} from '../src/core';
+
+describe('application logic', () => {
+  describe('setEntries', () => {
+    it('adds the entries to the state', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+  });
+});
