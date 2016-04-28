e58346a6f15343ab460bc6f4d25a6a84cbc6d701
Aayush Kapoor <xeoneux@icloud.com>
2016-04-29 01:58:58 +0530

    Add logic to vote

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 import {List, Map} from 'immutable';
 
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
@@ -38,6 +38,53 @@ describe('application logic', () => {
           pair: List.of('Trainspotting', '28 Days Later')
         }),
         entries: List.of('Sunshine')
+      }));
+    });
+  });
+
+  describe('vote', () => {
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 1
+          })
+        }),
+        entries: List()
+      }));
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Trainspotting');
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
       }));
     });
   });
