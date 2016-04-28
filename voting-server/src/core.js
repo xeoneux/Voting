682992e80b69dc97db2b5a11ab910445f2b7af7d
Aayush Kapoor <xeoneux@icloud.com>
2016-04-29 03:40:20 +0530

    Add logic to move to next pair

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -4,8 +4,22 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+  if (!vote) return [];
+
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+
+  if (aVotes > bVotes) return [a];
+  if (bVotes > aVotes) return [b];
+
+  return [a, b];
+}
+
 export function next(state) {
-  const entries = state.get('entries');
+  const entries = state.get('entries')
+    .concat(getWinners(state.get('vote')));
 
   return state.merge({
     vote: Map({ pair: entries.take(2) }),
