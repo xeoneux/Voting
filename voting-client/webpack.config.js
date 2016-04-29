7620c26be3b3aed084bd5c99fb6745f84aac3b55
Aayush Kapoor <xeoneux@icloud.com>
2016-04-29 11:58:34 +0530

    Add babel

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   entry: ['./src/index.js'],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extentions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
