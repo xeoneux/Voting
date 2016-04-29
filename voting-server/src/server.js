12add924c9b97e85514757a5974941c7b04e9b43
Aayush Kapoor <xeoneux@icloud.com>
2016-04-29 11:38:05 +0530

    Broadcast state to listener

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,5 +1,13 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  store.subscribe(() => {
+    io.emit('state', store.getState().toJS());
+  });
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+  });
 }
