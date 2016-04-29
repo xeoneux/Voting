41af27ef727e96e6ace701d6a06a328f49b83044
Aayush Kapoor <xeoneux@icloud.com>
2016-04-29 11:45:07 +0530

    Add seed data

--- voting-server/index.js
+++ voting-server/index.js
@@ -3,3 +3,9 @@ import startServer from './src/server';
 
 export const store = makeStore();
 startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({ type: 'NEXT' });
--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -9,5 +9,6 @@ export default function startServer(store) {
 
   io.on('connection', (socket) => {
     socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
   });
 }
