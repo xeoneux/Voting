df3ae9e3ddbbbf1128ca4deb4378d29e16661823
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 00:16:02 +0530

    Add react and react hot loader

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,11 +1,17 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'react-hot!babel'
       }
     ]
   },
@@ -18,6 +24,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
