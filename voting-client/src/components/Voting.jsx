7a663722c4073e8f1a4f8fd5273b967334516eb9
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 00:29:53 +0530

    Add voting component

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default React.createClass({
+  getPair: function () {
+    return this.props.pair || [];
+  },
+  render: function () {
+    return (
+      <div className="voting">
+        {
+          this.getPair().map(entry => {
+            return (
+              <button key={entry}>
+                <h1>{entry}</h1>
+              </button>
+            );
+          })
+        }
+      </div>
+    );
+  }
+});
