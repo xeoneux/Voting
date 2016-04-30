4f71f7d059fc01986840f5bdee66b159b15029db
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 00:29:38 +0530

    Update index file

---
+++ voting-client/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Voting from './components/Voting.jsx';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <Voting pair={pair} />,
+  document.getElementById('app')
+);
