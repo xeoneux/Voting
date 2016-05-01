6eda3edc03b3c5090e61a6bbe866a993646003c9
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 10:16:02 +0530

    Update results component

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -3,9 +3,33 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
+  getPair: function () {
+    return this.props.pair || [];
+  },
+  getVotes: function (entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  },
   render: function () {
     return (
-      <div>Hello from results!</div>
+      <div className="results">
+        {
+          this.getPair().map(entry => {
+            return (
+              <div key={entry} className="entry">
+                <h1>{entry}</h1>
+                <div className="voteCount">
+                  {
+                    this.getVotes(entry)
+                  }
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
     );
   }
 });
