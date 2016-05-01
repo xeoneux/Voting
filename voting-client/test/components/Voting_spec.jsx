b911c0142fd8c7d006d595a4538a0f899ad0a541
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 07:55:17 +0530

    Add more tests to voting component

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -33,4 +33,39 @@ describe('Voting', () => {
 
     expect(votedWith).to.equal('Trainspotting');
   });
+
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting hasVoted="Trainspotting"
+        pair={["Trainspotting", "28 Days Later"]} />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+
+  it('adds label to the voted entry', () => {
+    const component = renderIntoDocument(
+      <Voting hasVoted="Trainspotting"
+        pair={["Trainspotting", "28 Days Later"]} />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons[0].textContent).to.contain('Voted');
+  });
+
+  it('renders just the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Voting winner="Trainspotting" />
+    );
+
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons.length).to.equal(0);
+
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
+  });
 });
