dba50f8492ed0f5bc354e49639281030ab672b46
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 07:43:04 +0530

    Separate vote component

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Winner from './Winner.jsx';
+import Vote from './Vote.jsx';
 
 export default React.createClass({
   getPair: function () {
@@ -18,21 +19,7 @@ export default React.createClass({
         {
           this.props.winner
             ? (<Winner ref="winner" winner={this.props.winner} />)
-            : this.getPair().map(entry => {
-              return (
-                <button key={entry}
-                  onClick={
-                    () => this.props.vote(entry)
-                  }>
-                  <h1>{entry}</h1>
-                  {
-                    this.hasVotedFor(entry)
-                      ? (<div className="label">Voted</div>)
-                      : null
-                  }
-                </button>
-              );
-            })
+            : (<Vote {...this.props} />)
         }
       </div>
     );
