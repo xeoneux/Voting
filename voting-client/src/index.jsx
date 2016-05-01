d56f3baf70748f14d8099dcbf70cf721db86f368
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 13:55:27 +0530

    Add redux

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, hashHistory} from 'react-router';
+import {createStore} from 'redux';
 
+import reducer from './reducer';
 import App from './components/App.jsx';
 import Voting from './components/Voting.jsx';
 import Results from './components/Results.jsx';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: { Sunshine: 2 }
+    }
+  }
+});
 
 const routes = (
   <Route component={App}>
