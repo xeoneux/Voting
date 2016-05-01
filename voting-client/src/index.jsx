a2f8d14e4c2fd311b5cb744b72f90908f1fcd918
Aayush Kapoor <xeoneux@icloud.com>
2016-05-01 10:10:46 +0530

    Add results component

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -4,12 +4,14 @@ import {Route, Router, hashHistory} from 'react-router';
 
 import App from './components/App.jsx';
 import Voting from './components/Voting.jsx';
+import Results from './components/Results.jsx';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = (
   <Route component={App}>
     <Route path="/" component={Voting} />
+    <Route path="/results" component={Results} />
   </Route>
 );
 
