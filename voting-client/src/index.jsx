3223a44bcf0c0c2fd09ee24db9444c467be16bba
Aayush Kapoor <xeoneux@icloud.com>
2016-05-02 17:45:16 +0530

    Add socket.io client

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Route, Router, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 
 import reducer from './reducer';
 import App from './components/App.jsx';
@@ -19,6 +20,8 @@ store.dispatch({
     }
   }
 });
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
 
 const routes = (
   <Route component={App}>
