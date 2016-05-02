569942773bcd537873ea99f205e544fb04405024
Aayush Kapoor <xeoneux@icloud.com>
2016-05-02 17:47:50 +0530

    Add vote action

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -22,6 +22,9 @@ store.dispatch({
 });
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({ type: 'SET_STATE', state })
+);
 
 const routes = (
   <Route component={App}>
