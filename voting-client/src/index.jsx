98354fbc0b6c8df6b8a4b779aa5c7c4f88581fc4
Aayush Kapoor <xeoneux@icloud.com>
2016-05-03 17:17:40 +0530

    Add middleware to redux

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, Router, hashHistory} from 'react-router';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import io from 'socket.io-client';
 
@@ -10,17 +10,12 @@ import App from './components/App.jsx';
 import {VotingContainer} from './components/Voting.jsx';
 import {ResultsContainer} from './components/Results.jsx';
 import {setState} from './action_creators.jsx';
+import remoteActionMiddleware from './remote_action_middleware';
 
-const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: { Sunshine: 2 }
-    }
-  }
-});
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
